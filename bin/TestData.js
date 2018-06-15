f000810aaf154d0f107c14c88685a2244fc9963e
Robert Weigel <rweigel@gmu.edu>
2018-06-15 08:14:52 -0400

    Allow all params
    Former-commit-id: a867c58f787ac290a8897a463fa61f816d460444
    Former-commit-id: f7636cb4abc21389c4f64ddafc5f25572726c375

--- bin/TestData.js
+++ bin/TestData.js
@@ -9,7 +9,15 @@ var argv    = require('yargs')
 				})
 				.argv;
 
-var parameters = argv.parameters.split(",");
+var all = false;
+if (typeof(argv.parameters) === 'boolean') {
+	// Command line was --parameters
+	// with no parameters.
+	all = true;
+	var parameters = [""];
+} else {
+	var parameters = argv.parameters.split(",");
+}
 var start = argv.start.replace("Z","");
 var stop  = argv.stop.replace("Z","");
 var id    = argv.id;
@@ -38,58 +46,57 @@ scalarcats = [0,1,2];
 
 for (var i = startsec; i < stopsec; i++) {
 	var record = "";
-	if (parameters.includes('Time')) {
-		record = (new Date(i*1000).toISOString());
-	}
-	if (parameters.includes('scalar')) {
+	
+	record = (new Date(i*1000).toISOString());
+	if (all || parameters.includes('scalar')) {
 		record = record + "," + Math.sin(Math.PI*i/600);
 	}
-	if (parameters.includes('scalarint')) {
+	if (all || parameters.includes('scalarint')) {
 		record = record + "," + Math.round(1000*Math.sin(Math.PI*i/600));
 	}
-	if (parameters.includes('scalarstr')) {
+	if (all || parameters.includes('scalarstr')) {
 		record = record + "," + scalarstrs[(i-startsec) % scalarstrs.length];
 	}
-	if (parameters.includes('scalarcats')) {
+	if (all || parameters.includes('scalarcats')) {
 		record = record + "," + scalarcats[(i-startsec) % scalarcats.length];
 	}
-	if (parameters.includes('scalariso')) {
+	if (all || parameters.includes('scalariso')) {
 		record = record + "," + (new Date((i+1)*1000).toISOString()).slice(0,-5) + "Z";
 	}
-	if (parameters.includes('scalarmulti')) {
+	if (all || parameters.includes('scalarmulti')) {
 		record = record + "," + Math.sin(Math.PI*i/600);
 	}
-	if (parameters.includes('vector')) {
+	if (all || parameters.includes('vector')) {
 		record = record 
 					+ "," + Math.sin(Math.PI*(i-startsec)/600) 
 					+ "," + Math.sin(Math.PI*(i-startsec-150)/600) 
 					+ "," + Math.sin(Math.PI*(i-startsec-300)/600)
 	}
-	if (parameters.includes('vectorint')) {
+	if (all || parameters.includes('vectorint')) {
 		record = record 
 					+ "," + Math.round(1000*Math.sin(Math.PI*i/600))
 					+ "," + Math.round(1000*Math.sin(Math.PI*i/600))
 					+ "," + Math.round(1000*Math.sin(Math.PI*i/600));
 	}
-	if (parameters.includes('vectorstr')) {
+	if (all || parameters.includes('vectorstr')) {
 		record = record 
 						+ "," + scalarstrs[(i-startsec) % scalarstrs.length]
 						+ "," + scalarstrs[(i-startsec+1) % scalarstrs.length]
 						+ "," + scalarstrs[(i-startsec+2) % scalarstrs.length];
 	}
-	if (parameters.includes('vectorcats')) {
+	if (all || parameters.includes('vectorcats')) {
 		record = record 
 					+ "," + scalarcats[(i-startsec)   % scalarcats.length]
 					+ "," + scalarcats[(i-startsec+1) % scalarcats.length]
 					+ "," + scalarcats[(i-startsec+2) % scalarcats.length];
 	}
-	if (parameters.includes('vectoriso')) {
+	if (all || parameters.includes('vectoriso')) {
 		record = record 
 					+ "," + (new Date((i+1)*1000).toISOString()).slice(0,-5) + "Z"
 					+ "," + (new Date((i+2)*1000).toISOString()).slice(0,-5) + "Z"
 					+ "," + (new Date((i+3)*1000).toISOString()).slice(0,-5) + "Z"	;
 	}
-	if (parameters.includes('vectormulti')) {
+	if (all || parameters.includes('vectormulti')) {
 		record = record 
 					+ "," + Math.sin(Math.PI*(i-startsec)/600) 
 					+ "," + Math.sin(Math.PI*(i-startsec-150)/600) 
@@ -98,34 +105,34 @@ for (var i = startsec; i < stopsec; i++) {
 					+ "," + Math.sin(Math.PI*(i-startsec-150)/600) 
 					+ "," + Math.sin(Math.PI*(i-startsec-300)/600)
 	}
-	if (parameters.includes('transform')) {
+	if (all || parameters.includes('transform')) {
 		for (var j = 0;j < 9;j++) {
 			record = record + "," + Math.sin((Math.PI/300)*(i-startsec)/(j+1));
 		}
 	}
-	if (parameters.includes('transformmulti')) {
+	if (all || parameters.includes('transformmulti')) {
 		for (var j = 0;j < 9;j++) {
 			record = record + "," + Math.sin((Math.PI/300)*(i-startsec)/(j+1));
 		}
 	}
-	if (parameters.includes('spectra')) {
+	if (all || parameters.includes('spectra')) {
 		record = record + "," + 0; // f = 0 bin.
 		for (var j = 1;j < 10;j++) {
 			record = record + "," + 1/j;
 		}
 	}
-	if (parameters.includes('spectranobins')) {
+	if (all || parameters.includes('spectranobins')) {
 		for (var j = 0;j < 10;j++) {
 			record = record + "," + j;
 		}
 	}
-	if (parameters.includes('spectralarge')) {
+	if (all || parameters.includes('spectralarge')) {
 		record = record + "," + 0; // f = 0 bin.
 		for (var j = 1;j < 100;j++) {
 			record = record + "," + 1/j;
 		}
 	}
-	if (parameters.includes('spectramulti')) {
+	if (all || parameters.includes('spectramulti')) {
 		record = record + "," + 0; // f = 0 bin.
 		for (var j = 1;j < 10;j++) {
 			record = record + "," + 1/j;
