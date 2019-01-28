abc95b0d194ec10c7a89e430886876837fd54148
Robert Weigel <rweigel@gmu.edu>
2019-01-28 10:40:42 -0500

    Release v0.9.4

--- pkg/python.py
+++ pkg/python.py
@@ -1,5 +1,14 @@
 # If file given on command line, eval it.
 
+#import sys
+#sys.path.insert(0, 'example.zip')
+
 # If file not given on command line, drop to shell.
-import code
-code.interact(local=locals())
+#import code
+#code.interact(local=locals())
+
+with open('script.py', 'r') as myfile:
+  code = myfile.read()
+
+exec(code,globals(),locals())
+
