ed54087f0a6b2c6f9df1edc1f1f470d7b5540893
weigel <rweigel@gmu.edu>
2019-09-22 14:36:45 -0400

    Support HAPI 3.0 JSON ref and time-varying params

--- server.js
+++ server.js
@@ -93,75 +93,105 @@ if (typeof(FILE) == 'string') {
 	FILES = FILE;
 }
 
-CATALOGS = [];
-PREFIXES = [];
-for (var i = 0; i < FILES.length; i++) {
-	json = prepmetadata(FILES[i],HAPIVERSION,FORCE,VERIFIER,PLOTSERVER);
-	CATALOGS[i] = json['server']['id'];
-	PREFIXES[i] = json['server']['prefix'];
-}
-
 exceptions(); // Catch common start-up exceptions
 
-app.use(compress()); // Compress responses using gzip
+// Populate metadata.cache array, which has elements of catalog objects
+prepmetadata(FILES,HAPIVERSION,FORCE,VERIFIER,PLOTSERVER,main);
 
-// Log all requests, then call next route handler
-app.get('*', function (req,res,next) {
-	logreq(req);
-	next();
-})
+function main() {
 
-if (CATALOGS.length > 1) {
-	// TODO: Move file read to metadata?
-	html = fs
-			.readFileSync(__dirname + "/public/multiple.htm","utf8")
-			.toString()
-			.replace(/__CATALOG_LIST__/, JSON.stringify(CATALOGS));
+	let CATALOGS = [];
+	let PREFIXES = [];
 
-	app.get('/', function (req,res) {res.send(html);});
+	let i = 0;
+	for (let key in metadata.cache) {
+		CATALOGS[i] = metadata.cache[key]['server']['id'];
+		PREFIXES[i] = metadata.cache[key]['server']['prefix'];
+		i = i + 1;
+	}
+
+	app.use(compress()); // Compress responses using gzip
+
+	// Log all requests, then call next route handler
+	app.get('*', function (req,res,next) {
+		logreq(req);
+		next();
+	})
+
+	if (CATALOGS.length > 1) {
+		// TODO: Move file read to metadata?
+		html = fs
+				.readFileSync(__dirname + "/public/multiple.htm","utf8")
+				.toString()
+				.replace(/__CATALOG_LIST__/, JSON.stringify(CATALOGS));
+
+		app.get('/', function (req,res) {res.send(html);});
+	}
+
+	apiInit(CATALOGS,PREFIXES);
+
+	// TODO: This should be a callback to apiInit.
+	app.listen(argv.port, function () {
+
+		// Show messages after server started
+		if (CATALOGS.length == 1) {
+			var url = 'http://localhost:' + argv.port + PREFIXES[0] + "/hapi";
+		} else {
+			var url = 'http://localhost:' + argv.port;
+			console.log(ds() + "HAPI server list at http://localhost:" + argv.port);
+			for (var i = 0;i < CATALOGS.length;i++) {
+				console.log(ds() + "  http://localhost:" + argv.port + PREFIXES[i] + "/hapi");
+			}
+		}
+
+		if (OPEN) {
+			// Open browser window
+			var start = (process.platform == 'darwin'? 'open': process.platform == 'win32'? 'start': 'xdg-open');
+			require('child_process').exec(start + ' ' + url);
+		}
+
+		console.log(ds() + clc.blue("Listening on port " + argv.port));
+
+		if (TEST) {
+			// Exits with signal 0 or 1
+			test.urls(CATALOGS,PREFIXES,url,TEST);
+		}
+		if (VERIFY) {
+			// Exits with signal 0 or 1
+			verify(url);
+		}
+	});
 }
 
-for (var i = 0;i < CATALOGS.length;i++) {
-	console.log(ds() + clc.green("Initializing http://localhost:" + argv.port + PREFIXES[i] + "/hapi"));
+function apiInit(CATALOGS,PREFIXES,i) {
+
+	if (arguments.length == 2) {
+		i = 0;
+	}
+
+	if (i == CATALOGS.length) {
+		// Finalize API.
+		// The following must occur after last app.get() call.
+		// Any requests not matching set app.get() patterns will see error response.
+		app.get('*', function(req, res) {
+			if (PREFIXES.length == 1) {
+				res.send("Invalid URL. See <a href='"+PREFIXES[0]+"/hapi'>"+PREFIXES[0].substr(1)+"/hapi</a>");
+			} else {
+				res.send("Invalid URL. See <a href='/'>start page</a>");
+			}
+		});
+
+		// Handle uncaught errors in API request code.
+		app.use(errorHandler);
+		return;
+	}
+
+	let CATALOG = CATALOGS[i];
+	let PREFIX = PREFIXES[i]
+
+	console.log(ds() + clc.green("Initializing http://localhost:" + argv.port + PREFIX + "/hapi"));
 	console.log(ds() + "To run test URLs, use the --test option");
 	console.log(ds() + "To run verification tests, use the --verify option");
-
-	// Initialize the REST API for each catalog.
-	apiInit(CATALOGS[i],PREFIXES[i],i == CATALOGS.length-1);	
-}
-
-app.listen(argv.port, function () {
-
-	// Show messages after server started
-	if (CATALOGS.length == 1) {
-		var url = 'http://localhost:' + argv.port + PREFIXES[0] + "/hapi";
-	} else {
-		var url = 'http://localhost:' + argv.port;
-		console.log(ds() + "HAPI server list at http://localhost:" + argv.port);
-		for (var i = 0;i < CATALOGS.length;i++) {
-			console.log(ds() + "  http://localhost:" + argv.port + PREFIXES[i] + "/hapi");
-		}
-	}
-
-	if (OPEN) {
-		// Open browser window
-		var start = (process.platform == 'darwin'? 'open': process.platform == 'win32'? 'start': 'xdg-open');
-		require('child_process').exec(start + ' ' + url);
-	}
-
-	console.log(ds() + clc.blue("Listening on port " + argv.port));
-
-	if (TEST) {
-		// Exits with signal 0 or 1
-		test.urls(CATALOGS,PREFIXES,url,TEST);
-	}
-	if (VERIFY) {
-		// Exits with signal 0 or 1
-		verify(url);
-	}
-});
-
-function apiInit(CATALOG,PREFIX,last) {
 
 	// Serve static files in ./public
 	app.use(PREFIX, express.static(__dirname + '/public'));
@@ -209,31 +239,16 @@ function apiInit(CATALOG,PREFIX,last) {
 		cors(res);
 		res.contentType("application/json");
 
-		// Check for required id parameter
-		if (!req.query.id) {
-			error(req,res,1400,"A dataset id must be given.");
-			return;
-		}
-
-		// Check if extra parameters given
-		for (var key in req.query) {
-			if (!["id","parameters"].includes(key)) {
-				error(req,res,1401,"'id' and 'parameters' are the only valid query parameters.");
-				return;
-			}
-		}	
-
-		// Check if id is valid
-		if (!idCheck(CATALOG,req.query.id)) {
-			error(req,res,1406);
-			return;
+		// Check query string and set defaults as needed.
+		if (!queryCheck(req,res,CATALOG,'info')) {
+			return; // Error already sent, so return;
 		}
 
 		// Get subsetted info response based on requested parameters.
 		// info() returns integer error code if error.
 		// TODO: Reconsider this interface to info() - 
 		// infoCheck() is more consistent with other code.
-		var header = info(CATALOG,req,res); 
+		var header = info(req,res,CATALOG); 
 		if (typeof(header) === "number") {
 			error(req,res,header,"At least one parameter not found in dataset.");
 			return;
@@ -248,39 +263,13 @@ function apiInit(CATALOG,PREFIX,last) {
 
 		cors(res);
 
-		// TODO: This is duplicate code from /info
-		if (!req.query.id) {
-			error(req,res,1400,"A dataset id must be given.");
-			return;
-		}
-
-		// Check if id is valid
-		// TODO: Duplicate code from /info
-		if (!idCheck(CATALOG,req.query.id)) {
-			error(req,res,1406);
-			return;
-		}
-
-		// Check if query parameters are all valid
-		var allowed = ["id","parameters","time.min","time.max","format","include","attach"];
-		for (var key in req.query) {
-			if (!allowed.includes(key)) {
-				error(req,res,1401,"The only allowed query parameters are " + allowed.join(", "));
-				return;
-			}
-		}	
-
-		if (!req.query["time.min"]) {
-			error(req,res,1402,"time.min is required");
-			return;
-		}
-		if (!req.query["time.max"]) {
-			error(req,res,1403,"time.max is required");
-			return;
+		// Check query string and set defaults as needed.
+		if (!queryCheck(req,res,CATALOG,'data')) {
+			return; // Error already sent, so return;
 		}
 
 		// Get subsetted /info response based on requested parameters.
-		var header = info(CATALOG,req,res);
+		var header = info(req,res,CATALOG);
 		if (typeof(header) === "number") {
 			// One or more of the requested parameters are invalid.
 			error(req,res,header,"At least one parameter not found in dataset.");
@@ -347,20 +336,7 @@ function apiInit(CATALOG,PREFIX,last) {
 		data(req,res,CATALOG,header,include);
 	})
 
-	// The following must always be after last app.get() statement.
-	// Any requests not matching above patterns will trigger 
-	// errorHandler() call.
-	if (last) {
-		// Fall through
-		app.get('*', function(req, res) {
-			if (PREFIXES.length == 1) {
-				res.send("Invalid URL. See <a href='"+PREFIX+"/hapi'>"+PREFIX.substr(1)+"/hapi</a>");
-			} else {
-				res.send("Invalid URL. See <a href='/'>start page</a>");
-			}
-		});
-		app.use(errorHandler);
-	}
+	apiInit(CATALOGS,PREFIXES,++i);
 }
 
 function cors(res) {
@@ -370,45 +346,16 @@ function cors(res) {
 	res.header('Access-Control-Allow-Headers', 'Content-Type');
 }
 
-function normalizeTime(timestr) {
-
-	// Convert to YYYY-MM-DDTHH:MM:SS.FFFFFFFFFZ 
-	// (nanosecond precision). All command line programs
-	// will be given this format.
-
-	// Need to extract it here and then insert at end
-	// because moment.utc(timestr).toISOString()
-	// ignores sub-millisecond parts of time string.
-	var re = new RegExp(/.*\.[0-9]{3}([0-9].*)Z/);
-	var submilli = "000000";
-	if (re.test(timestr)) {
-		var submilli = timestr.replace(/.*\.[0-9]{3}([0-9].*)Z$/,"$1");
-		var pad = "0".repeat(6-submilli.length);
-		submilli = submilli + pad; 
-	}
-
-	if (/^[0-9]{4}Z$/.test(timestr)) {
-		timestr = timestr.slice(0,-1) + "-01-01T00:00:00.000Z";
-	}
-	if (/^[0-9]{4}-[0-9]{2}Z$/.test(timestr)) {
-		timestr = timestr.slice(0,-1) + "-01T00:00:00.000Z";
-	}
-	if (/^[0-9]{4}-[0-9]{3}Z$/.test(timestr)) {
-		timestr = timestr.slice(0,-1) + "T00:00:00.000Z";
-	}
-	if (/^[0-9]{4}-[0-9]{2}-[0-9]{2}Z$/.test(timestr)) {
-		timestr = timestr.slice(0,-1) + "T00:00:00.000Z";
-	}
-	timestr = moment.utc(timestr).toISOString();
-	timestr = timestr.slice(0,-1) + submilli + "Z";
-	return timestr;
-}
-
-function info(catalog,req,res) {
+function info(req,res,catalog) {
 
 	// Read parameter metadata.
-	// Parse string metadata because (json will be modified).
-	json = metadata(catalog,'info',req.query.id)
+	if (req.query.resolve_references === "true") {
+		json = metadata(catalog,'info',req.query.id);
+	} else {
+		json = metadata(catalog,'info-raw',req.query.id);
+	}
+	
+	// Copy string metadata because (json will be modified).
 	var json = JSON.parse(JSON.stringify(json));
 
 	// Create array of known parameter names
@@ -417,7 +364,7 @@ function info(catalog,req,res) {
 		knownparams[i] = json.parameters[i].name;
 	}
 
-	// Create arrray from comma-separated parameters in query
+	// Create array from comma-separated parameters in query
 	var wantedparams = [];
 	if (req.query.parameters) {
 		wantedparams = req.query.parameters.split(",");
@@ -461,6 +408,7 @@ function info(catalog,req,res) {
 			delete json.parameters[i];
 		}
 	}
+
 	// Remove nulls placed when array element is deleted
 	json.parameters = json.parameters.filter(function(n){return n != undefined}); 
 
@@ -469,6 +417,40 @@ function info(catalog,req,res) {
 }
 
 function data(req,res,catalog,header,include) {
+
+	function normalizeTime(timestr) {
+
+		// Convert to YYYY-MM-DDTHH:MM:SS.FFFFFFFFFZ 
+		// (nanosecond precision). All command line programs
+		// will be given this format.
+
+		// Need to extract it here and then insert at end
+		// because moment.utc(timestr).toISOString()
+		// ignores sub-millisecond parts of time string.
+		var re = new RegExp(/.*\.[0-9]{3}([0-9].*)Z/);
+		var submilli = "000000";
+		if (re.test(timestr)) {
+			var submilli = timestr.replace(/.*\.[0-9]{3}([0-9].*)Z$/,"$1");
+			var pad = "0".repeat(6-submilli.length);
+			submilli = submilli + pad; 
+		}
+
+		if (/^[0-9]{4}Z$/.test(timestr)) {
+			timestr = timestr.slice(0,-1) + "-01-01T00:00:00.000Z";
+		}
+		if (/^[0-9]{4}-[0-9]{2}Z$/.test(timestr)) {
+			timestr = timestr.slice(0,-1) + "-01T00:00:00.000Z";
+		}
+		if (/^[0-9]{4}-[0-9]{3}Z$/.test(timestr)) {
+			timestr = timestr.slice(0,-1) + "T00:00:00.000Z";
+		}
+		if (/^[0-9]{4}-[0-9]{2}-[0-9]{2}Z$/.test(timestr)) {
+			timestr = timestr.slice(0,-1) + "T00:00:00.000Z";
+		}
+		timestr = moment.utc(timestr).toISOString();
+		timestr = timestr.slice(0,-1) + submilli + "Z";
+		return timestr;
+	}
 
 	var start = normalizeTime(req.query["time.min"]);
 	var stop = normalizeTime(req.query["time.max"]);
@@ -881,19 +863,63 @@ function csvTo(records,first,last,header,include) {
 	}
 }
 
-function idCheck(catalogid,datasetid) {
-	// Get list of datasets.
-	var catalog = metadata(catalogid,'catalog');
-	var datasets = catalog.catalog;
-	var found = false;
-	// Determine if requested id is in list
-	for (var i = 0;i < datasets.length;i++) {
-		if (datasets[i]['id'] === datasetid) {
-			return true;
-			break;
+function queryCheck(req, res, catalog, type) {
+
+	// Check query parameters for query of type='info' or 'data'
+	// If invalid query parameter, send error and return false
+	// If req.query.resolve_references === undefined, set to "true"
+
+	// Check for required id parameter
+	if (!req.query.id) {
+		error(req,res,1400,"A dataset id must be given.");
+		return false;
+	}
+
+	let ids = metadata(catalog,'ids');
+	if (!ids.includes(req.query.id)) {
+		error(req,res,1406);
+		return false;
+	}
+
+	if (type === 'info') {
+		// Check if extra parameters given
+		for (var key in req.query) {
+			if (!["id","parameters","resolve_references"].includes(key)) {
+				error(req,res,1401,"'id', 'parameters', and 'resolve_references' are the only valid query parameters.");
+				return false;
+			}
+		}
+	} else {
+		// Check if query parameters are all valid
+		var allowed = ["id","parameters","time.min","time.max","format","include","attach","resolve_references"];
+		for (var key in req.query) {
+			if (!allowed.includes(key)) {
+				error(req,res,1401,"The only allowed query parameters are " + allowed.join(", "));
+				return false;
+			}
+		}
+		if (!req.query["time.min"]) {
+			error(req,res,1402,"time.min is required");
+			return;
+		}
+		if (!req.query["time.max"]) {
+			error(req,res,1403,"time.max is required");
+			return;
 		}
 	}
-	return false;
+
+	// Set default resolve_references to be true if not given
+	if (req.query.resolve_references === undefined) {
+		req.query.resolve_references = "true";
+	}
+
+	// If resolve_references given, check that it is "true" or "false".
+	if (!["true","false"].includes(req.query.resolve_references)) {
+		error(req,res,1411,"resolve_references must be 'true' or 'false'");
+		return false;
+	}
+
+	return true;
 }
 
 function timeCheck(header) {
@@ -995,6 +1021,7 @@ function error(req,res,code,message) {
 		"1408": {status: 400, "message": "HAPI 1408: too much time or data requested"},
 		"1409": {status: 400, "message": "HAPI 1409: unsupported output format"},
 		"1410": {status: 400, "message": "HAPI 1410: unsupported include value"},
+		"1411": {status: 400, "message": "HAPI 1411: unsupported resolve_references value"},
 		"1500": {status: 500, "message": "HAPI 1500: internal server error"},
 		"1501": {status: 500, "message": "HAPI 1501: upstream request error"}
 	}
