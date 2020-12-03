c37181df8865e8bdfd2c2c6acf7fc3cbb28baee2
KSR <ksreddy9945@gmail.com>
2020-12-03 17:39:42 -0500

    fixed indentation

--- metadata/CAIO/CAIO2HAPI.js
+++ metadata/CAIO/CAIO2HAPI.js
@@ -5,12 +5,12 @@ const yargs  = require('yargs');
 const clc  = require('chalk');
 
 const argv = yargs
-  .option('file', {
-    description: 'File to Parse',
-   })
-  .help()
-  .alias('help', 'h')
-  .argv;
+                .option('file', {
+					description: 'File to Parse',
+				})
+                .help()
+                .alias('help', 'h')
+                .argv;
 
 var file_to_parse = path.join(__dirname, 'tmp', argv.file);
 var start_time = "";
@@ -18,107 +18,103 @@ var stop_time = "";
 
 //Gets the start_time and stop_time for the metadata from the JSON repsonse
 function extract_start_stop(callback){
-  let json_response= fs.readFileSync('json_response.json');
-  let array = JSON.parse(json_response);
-
-  for (item in array) {
-    for (subItem in array[item]) {
-      if( array[item][subItem]['DATASET.DATASET_ID'] === argv.file.replace('.CEF.XML','')) {
-          start_time = array[item][subItem]['DATASET.START_DATE'];
-          stop_time = array[item][subItem]['DATASET.END_DATE'];      
-          break;    
-      }
-    }
-  }
-  callback();
+	let json_response= fs.readFileSync('json_response.json');
+	let array = JSON.parse(json_response);
+	
+	for (item in array) {
+		for (subItem in array[item]) {
+			if( array[item][subItem]['DATASET.DATASET_ID'] === argv.file.replace('.CEF.XML','')) {
+				start_time = array[item][subItem]['DATASET.START_DATE'];
+				stop_time = array[item][subItem]['DATASET.END_DATE'];      
+				break;    
+			}
+		}
+	}
+	callback();
 }
 
 
 //Will parse each file sent as a argument and converts the XML into HAPI metadata format
 function parsing(){
-  if (fs.existsSync(file_to_parse)) {
-    fs.readFile(file_to_parse, (err, data) => {
-	    xml2js.parseString(data, (err, result) => { 
-        if(err) {
-          throw err;
-        }
-        res = JSON.stringify(result, null, 4);
-      });
-      const obj = JSON.parse(res);
-      var resobj = {
-      };
+	if (fs.existsSync(file_to_parse)) {
+		fs.readFile(file_to_parse, (err, data) => {
+			xml2js.parseString(data, (err, result) => { 
+				if(err) {
+					throw err;
+				}
+				res = JSON.stringify(result, null, 4);
+			});
+			const obj = JSON.parse(res);
+			var resobj = {
 
-    resobj.parameters = [];
-    var paramarray = obj.DATASETS.DATASET_METADATA;
+			};
+			
+			resobj.parameters = [];
+			var paramarray = obj.DATASETS.DATASET_METADATA;
+			if( paramarray[paramarray.length-1].PARAMETERS != undefined ){
+				paramarray[paramarray.length-1].PARAMETERS[0].PARAMETER.forEach(element => {
+					if(element.PARAMETER_ID === undefined || element.FIELDNAM ===  undefined || element.UNITS === undefined || element.FILLVAL === undefined || element.VALUE_TYPE === undefined ) {
+						console.log("Missing elements for "+ argv.file + " :");
+						
+						if(element.PARAMETER_ID === undefined ){
+							console.log(clc.yellow("name"));
+						} 
+						if(element.UNITS === undefined ){
+							console.log(clc.yellow("units"));
+						} 
+						if(element.FIELDNAM === undefined ){
+							console.log(clc.yellow("description"));
+						} 
+						if(element.FILLVAL === undefined ){
+							console.log(clc.yellow("fill"));
+						} 
+						if(element.VALUE_TYPE === undefined ){
+							console.log(clc.yellow("type"));
+						} 
+					}
 
-    if( paramarray[paramarray.length-1].PARAMETERS != undefined ){
-      paramarray[paramarray.length-1].PARAMETERS[0].PARAMETER.forEach(element => {
-
-       if(element.PARAMETER_ID === undefined || element.FIELDNAM ===  undefined || element.UNITS === undefined || element.FILLVAL === undefined || element.VALUE_TYPE === undefined ) {
-          console.log("Missing elements for "+ argv.file + " :");
-          if(element.PARAMETER_ID === undefined ){
-            console.log(clc.yellow("name"));
-          } 
-          if(element.UNITS === undefined ){
-            console.log(clc.yellow("units"));
-          } 
-          if(element.FIELDNAM === undefined ){
-            console.log(clc.yellow("description"));
-          } 
-          if(element.FILLVAL === undefined ){
-            console.log(clc.yellow("fill"));
-          } 
-          if(element.VALUE_TYPE === undefined ){
-            console.log(clc.yellow("type"));
-          } 
-          
-       }
-
-        var paramobj = {
-          name :element.PARAMETER_ID === undefined ? "" : element.PARAMETER_ID.toString(),
-          description: element.FIELDNAM ===  undefined ? "" : element.FIELDNAM.toString(),
-          units :  element.UNITS === undefined ? "" : element.UNITS.toString(),
-          fill : element.FILLVAL === undefined ? "" : element.FILLVAL.toString(),
-          type : element.VALUE_TYPE === undefined ? "" : element.VALUE_TYPE.toString()
-        };
-
-      resobj.parameters.push(paramobj);
-
-      });
-    } else {
-      console.log(clc.yellow("Warning: "+ argv.file + " does not have parameters!"))
-    }
-
-  TIME_RESOLUTION = "";
-    if (paramarray[paramarray.length-1].TIME_RESOLUTION) {
-      TIME_RESOLUTION = paramarray[paramarray.length-1].TIME_RESOLUTION.toString();
-    }
-    var buildobj = {
-      startDate : start_time,
-      stopDate : stop_time,
-      cadence : TIME_RESOLUTION,
-      description: paramarray[paramarray.length-1].DATASET_DESCRIPTION.toString(),
-      resourceURL : "https://csa.esac.esa.int/csa/aio/product-action?RETRIEVALTYPE=HEADER&DATASET_ID="+argv.file.replace('.CEF.XML',''),
-      contact : paramarray[paramarray.length-1].CONTACT_COORDINATES.toString(),
-      x_original_metadata: res,
-      parameters: resobj.parameters
-    }
-
-    var dir = './meta';
-
-    if (!fs.existsSync(dir)){
-      fs.mkdirSync(dir);
-    }
-
-    //Stores the resulting metadata created into the meta folder.
-    fs.writeFile (path.join(__dirname,"meta", argv.file.replace('.CEF.XML','.JSON')), JSON.stringify(buildobj, null, 4), function(err) {
-      if (err) throw err;
-    });
-
-	 });
-  }
+					var paramobj = {
+						name :element.PARAMETER_ID === undefined ? "" : element.PARAMETER_ID.toString(),
+						description: element.FIELDNAM ===  undefined ? "" : element.FIELDNAM.toString(),
+						units :  element.UNITS === undefined ? "" : element.UNITS.toString(),
+						fill : element.FILLVAL === undefined ? "" : element.FILLVAL.toString(),
+						type : element.VALUE_TYPE === undefined ? "" : element.VALUE_TYPE.toString()
+					};
+					
+					resobj.parameters.push(paramobj);
+				});
+			} else {
+				console.log(clc.yellow("Warning: "+ argv.file + " does not have parameters!"))
+			}
+			
+			TIME_RESOLUTION = "";
+			if (paramarray[paramarray.length-1].TIME_RESOLUTION) {
+				TIME_RESOLUTION = paramarray[paramarray.length-1].TIME_RESOLUTION.toString();
+			}
+			var buildobj = {
+				startDate : start_time,
+				stopDate : stop_time,
+				cadence : TIME_RESOLUTION,
+				description: paramarray[paramarray.length-1].DATASET_DESCRIPTION.toString(),
+				resourceURL : "https://csa.esac.esa.int/csa/aio/product-action?RETRIEVALTYPE=HEADER&DATASET_ID="+argv.file.replace('.CEF.XML',''),
+				contact : paramarray[paramarray.length-1].CONTACT_COORDINATES.toString(),
+				x_original_metadata: res,
+				parameters: resobj.parameters
+			}
+			
+			var dir = './meta';
+			if (!fs.existsSync(dir)){
+				fs.mkdirSync(dir);
+			}
+			
+			//Stores the resulting metadata created into the meta folder.
+			fs.writeFile (path.join(__dirname,"meta", argv.file.replace('.CEF.XML','.JSON')), JSON.stringify(buildobj, null, 4), function(err) {
+				if (err) throw err;
+			});
+		});
+	}
 	else {
 		console.log(clc.red("XML File not found to parse for "+argv.file.replace('.CEF.XML','')));
-  } 
+	} 
 }
 extract_start_stop(parsing);
--- metadata/CAIO/FILL_CATALOG.js
+++ metadata/CAIO/FILL_CATALOG.js
@@ -6,11 +6,12 @@ const fs = require('fs');
 const path = require('path');
 
 const argv = yargs
-              .option('update', 
-                  {description: 'Force update the existing metadata'})
-              .help()
-              .alias('help', 'h')
-              .argv;
+				.option('update', {
+					description: 'Force update the existing metadata'
+				})
+                .help()
+                .alias('help', 'h')
+                .argv;
 
 //var url = "https://csa.esac.esa.int/csa/aio/metadata-action?SELECTED_FIELDS=DATASET.DATASET_ID,DATASET.START_DATE,DATASET.END_DATE,DATASET.TITLE&RESOURCE_CLASS=TASET&RETURN_TYPE=JSON&QUERY=(DATASET.IS_CEF='true')"
 var url = 'https://csa.esac.esa.int/csa/aio/metadata-action?SELECTED_FIELDS=DATASET.DATASET_ID,DATASET.START_DATE,DATASET.END_DATE,DATASET.TITLE&RESOURCE_CLASS=DATASET&RETURN_TYPE=JSON';
@@ -23,118 +24,112 @@ var catalog = [];
 var id_arr = [];
 
 function fill_catalog(callback1){
-
-  console.log("Requesting " + url);
-
-  request(url, function (error, response, body) {
-
-    console.log("Received " + url);
-    if (error) {
-      console.log("Request to url :"+ url+ "Failed due to :"+ error);
-      process.exit(1);
-    }   
-
-    if (response && response.statusCode != 200) {
-      console.log("Request to url :" 
-                    + url
-                    + "Failed due to no response or non-200 status code.");
-      process.exit(1);
-    }
-
-    body = JSON.stringify(JSON.parse(body), null, 4);
-    // Saves the JSON response into a file for later extraction of start_time
-    // and stop_time while parsing the XML's
-    console.log("Writing json_response.json");
-    fs.writeFile(path.join(__dirname, "json_response.json"), body,
-      function(err) {
-        console.log("Wrote json_response.json");
-        if(err) {
-          return console.log(err);
-        }
-    });
-
-    console.log("Building catalog.")
-    var array = JSON.parse(body);
-    for (item in array) {
-      for (subItem in array[item]) {
-        var catalog_obj = {
-          "id": array[item][subItem]['DATASET.DATASET_ID'],
-          "title": array[item][subItem]['DATASET.TITLE']
-        }; 
-        id_arr.push(array[item][subItem]['DATASET.DATASET_ID'] );
-        catalog.push(catalog_obj);
-      }
-    }
-    callback1();
-    console.log("Built catalog.")
-  });
+	console.log("Requesting " + url);
+	request(url, function (error, response, body) {
+		console.log("Received " + url);
+		if (error) {
+			console.log("Request to url :"+ url+ "Failed due to :"+ error);
+			process.exit(1);
+		}   
+		if (response && response.statusCode != 200) {
+			console.log("Request to url :" 
+                            + url
+							+ "Failed due to no response or non-200 status code.");
+			process.exit(1);
+		}
+		
+		body = JSON.stringify(JSON.parse(body), null, 4);
+		// Saves the JSON response into a file for later extraction of start_time
+		// and stop_time while parsing the XML's
+		console.log("Writing json_response.json");
+		fs.writeFile(path.join(__dirname, "json_response.json"), body,
+		function(err) {
+			console.log("Wrote json_response.json");
+			if(err) {
+				return console.log(err);
+			}
+		});
+		
+		console.log("Building catalog.")
+		var array = JSON.parse(body);
+		for (item in array) {
+			for (subItem in array[item]) {
+				var catalog_obj = {
+					"id": array[item][subItem]['DATASET.DATASET_ID'],
+					"title": array[item][subItem]['DATASET.TITLE']
+				}; 
+				id_arr.push(array[item][subItem]['DATASET.DATASET_ID'] );
+				catalog.push(catalog_obj);
+			}
+		}
+		callback1();
+		console.log("Built catalog.")
+	});
 }
 
 fill_catalog(download_decompress);
 
 function download_decompress(){
-  
-  // Save the /catalog response content to a file.
-  console.log("Writing catalog_response.json");
-  fs.writeFile(path.join(__dirname, "catalog_response.json"), JSON.stringify(catalog, null, 4),
-      function(err) {
-        console.log("Wrote catalog_response.json");
-        if(err) {
-          return console.log(err);
-        }
-  });
-
-  //for(var i=1;i<id_arr.length;i++){
-  for(var i=1;i<100;i++){
-    id_arr[i] = id_arr[i].replace(/"/g, "");
-    const filePath = path.join(__dirname, 'tmp', id_arr[i].toString()+ ".CEF.XML");
-    // Downloads only if there is no exisitng metadata in the meta folder.
-    // Force downloads the metadata of --update argument passed on command line.
-    if(!fs.existsSync(filePath) || argv.update){
-      var url  = "curl 'https://csa.esac.esa.int/csa/aio/product-action?RETRIEVALTYPE=HEADER&DATASET_ID=" + id_arr[i] + "'";
-      var dir = './tmp';
-
-      if (!fs.existsSync(dir)){
-        fs.mkdirSync(dir);
-      }
-
-      var path1 = "'"+ __dirname + "/tmp/" +id_arr[i] + ".tar.gz'";
-      var cmd = url + ' > ' + path1.toString();
-      let child;
-      console.log("Executing " + url);
-      try {
-        child = require('child_process').spawnSync('sh', ['-c', cmd], {stdio: 'pipe'});
-        console.log("Executed " + cmd);
-      } catch (ex) {
-        console.log(ds() + clc.red("Could not execute command: " + cmd + "."));
-        if (ex.stderr) {
-          console.log(clc.red(ex.stderr.toString()));
-        }
-        if (exit_if_fail) {
-          process.exit(1);
-        } else {  
-          return false;
-        }
-      }
-
-      // Decompresses tar.gz content into the tmp folder
-      if (child.status == 0) {
-        var tgz_tmp = __dirname + "/tmp/";
-        var tgz = tgz_tmp+id_arr[i] +".tar.gz";
-        console.log("Extracting " + tgz + " to " + tgz_tmp);
-        targz.decompress({
-          src: tgz,
-          dest: tgz_tmp
-          }, function(err){
-            if (err) {
-              console.log(clc.red("Error in Decompressing "+id_arr[i] + err));
-            } 
-            console.log("Extracted " + tgz + " to " + tgz_tmp);
-        });
-      }
-    }
-    else {
-      console.log("Metadata file already exists for " + id_arr[i] + ". Will not redownload.");
-    }
-  } 
+	// Save the /catalog response content to a file.
+	console.log("Writing catalog_response.json");
+	fs.writeFile(path.join(__dirname, "catalog_response.json"), JSON.stringify(catalog, null, 4),
+	function(err) {
+		console.log("Wrote catalog_response.json");
+		if(err) {
+			return console.log(err);
+		}
+	});
+	
+	//for(var i=1;i<id_arr.length;i++){
+	for(var i=1;i<100;i++){
+		id_arr[i] = id_arr[i].replace(/"/g, "");
+		const filePath = path.join(__dirname, 'tmp', id_arr[i].toString()+ ".CEF.XML");
+		// Downloads only if there is no exisitng metadata in the meta folder.
+		// Force downloads the metadata of --update argument passed on command line.
+		if(!fs.existsSync(filePath) || argv.update){
+			var url  = "curl 'https://csa.esac.esa.int/csa/aio/product-action?RETRIEVALTYPE=HEADER&DATASET_ID=" + id_arr[i] + "'";
+			var dir = './tmp';
+			
+			if (!fs.existsSync(dir)){
+				fs.mkdirSync(dir);
+			}	
+			var path1 = "'"+ __dirname + "/tmp/" +id_arr[i] + ".tar.gz'";
+			var cmd = url + ' > ' + path1.toString();
+			let child;
+			console.log("Executing " + url);
+			try {
+				child = require('child_process').spawnSync('sh', ['-c', cmd], {stdio: 'pipe'});
+				console.log("Executed " + cmd);
+			} catch (ex) {
+				console.log(ds() + clc.red("Could not execute command: " + cmd + "."));
+				if (ex.stderr) {
+					console.log(clc.red(ex.stderr.toString()));
+				}
+				if (exit_if_fail) {
+					process.exit(1);
+				} else {  
+					return false;
+				}
+			}
+			
+			// Decompresses tar.gz content into the tmp folder
+			if (child.status == 0) {
+				var tgz_tmp = __dirname + "/tmp/";
+				var tgz = tgz_tmp+id_arr[i] +".tar.gz";
+				console.log("Extracting " + tgz + " to " + tgz_tmp);
+				targz.decompress({
+					src: tgz,
+					dest: tgz_tmp
+				}, function(err){
+					if (err) {
+						console.log(clc.red("Error in Decompressing "+id_arr[i] + err));
+					} 
+					console.log("Extracted " + tgz + " to " + tgz_tmp);
+				});
+			}
+		}
+		else {
+			console.log("Metadata file already exists for " + id_arr[i] + ". Will not redownload.");
+		}
+	} 
 }
