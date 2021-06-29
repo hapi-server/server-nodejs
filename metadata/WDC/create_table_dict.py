8330351dae5d78ea708ead729330624d4b76b30d
Srikar Reddy Karemma <srikar_reddy@Srikars-MacBook-Pro.local>
2021-06-30 00:19:57 +0530

    added code for creation of table dict

---
+++ metadata/WDC/create_table_dict.py
@@ -0,0 +1,25 @@
+from magpy.stream import *
+import os
+
+from fnmatch import fnmatch
+
+root = os.getcwd() + "/obsdata/1minval"
+pattern = "*.wdc"
+
+table_dict = {}
+
+for path, subdirs, files in os.walk(root):
+    for name in files:
+        if fnmatch(name, pattern):
+            name = os.path.join(path, name)
+            data = read(os.path.join(path, name))
+            print(os.path.join(path, name))
+            print(data.header)
+            print()
+            print()
+            if data.header['StationID'] not in table_dict.keys():
+                table_dict[data.header['StationID']] = {'XYZF': [], 'HDZF': []}
+
+            table_dict[data.header['StationID']][data.header['DataComponents']].append(
+                name[-17:-13] + '-' + name[-7:-5])
+print(table_dict)
