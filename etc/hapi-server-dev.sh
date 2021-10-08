54d505e8289b93348e4907bddb14646cac324104
Ubuntu <rweigel@gmu.edu>
2021-10-08 17:22:56 +0000

    server config

--- etc/hapi-server-dev.sh
+++ etc/hapi-server-dev.sh
@@ -6,8 +6,11 @@
 
 /home/ubuntu/.nvm/versions/node/v8.17.0/bin/node \
     /home/ubuntu/server-nodejs-dev/server.js \
-    --port 8998 \
-    --ignore \
+    --port 8998 --ignore \
     -f /home/ubuntu/server-nodejs-dev/metadata/TestData2.0.json \
-    -f /home/ubuntu/server-nodejs-dev/metadata/INTERMAGNET.json \    
+    -f /home/ubuntu/server-nodejs-dev/metadata/TestData2.1.json \
+    -f /home/ubuntu/server-nodejs-dev/metadata/TestData3.0.json \
+    -f /home/ubuntu/server-nodejs-dev/metadata/TestData3.1.json \
+    -f /home/ubuntu/server-nodejs-dev/metadata/INTERMAGNET.json \
+    -f /home/ubuntu/server-nodejs-dev/metadata/CAIO.json \        
     2>&1
