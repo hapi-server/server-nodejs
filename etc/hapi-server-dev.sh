a60859c72381ab221da77d543be7af67363f647e
Ubuntu <rweigel@gmu.edu>
2023-02-06 01:11:31 +0000

    show all in dev ui

--- etc/hapi-server-dev.sh
+++ etc/hapi-server-dev.sh
@@ -7,7 +7,7 @@ APPDIR=/home/ubuntu/server-nodejs-dev
     /home/ubuntu/server-nodejs-dev/server.js \
     --port 8998 --ignore \
     --server-ui-include https://raw.githubusercontent.com/hapi-server/servers/master/dev.txt \
-    -f $APPDIR/metadata/SSCWeb.json \
+    --server-ui-include https://raw.githubusercontent.com/hapi-server/servers/master/all_.txt \
     -f $APPDIR/metadata/TestData2.0.json \
     -f $APPDIR/metadata/TestData2.1.json \
     -f $APPDIR/metadata/TestData3.0.json \
