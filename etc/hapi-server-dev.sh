17094850f424b39465d7e70244a4dab12e6f22fb
Ubuntu <rweigel@gmu.edu>
2023-03-22 01:30:40 +0000

    cpulimit

--- etc/hapi-server-dev.sh
+++ etc/hapi-server-dev.sh
@@ -2,7 +2,8 @@
 
 APPDIR=/home/ubuntu/server-nodejs-dev
 
-/home/ubuntu/.nvm/versions/node/v8.17.0/bin/node \
+#cpulimit -l 5 -- nice -20 \
+/home/ubuntu/.nvm/versions/node/v18.10.0/bin/node \
     --max-old-space-size=256 \
     $APPDIR/server.js \
     --port 8998 --ignore \
@@ -13,6 +14,7 @@ APPDIR=/home/ubuntu/server-nodejs-dev
     -f $APPDIR/metadata/TestData3.0.json \
     -f $APPDIR/metadata/TestData3.1.json \
     -f $APPDIR/metadata/URLWatcher.json \
+    -f $APPDIR/metadata/SSCWeb-chunk-ltfloats-parallel.json \
     -f $APPDIR/metadata/Example0.json \
     -f $APPDIR/metadata/Example2.json \
     -f $APPDIR/metadata/CAIO.json \        
