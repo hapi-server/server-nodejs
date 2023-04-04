bf6f6788518737d28b7ea613f632bdb8ab0bfe3a
Ubuntu <rweigel@gmu.edu>
2023-04-04 12:36:49 +0000

    cpu limits

--- etc/hapi-server-dev.sh
+++ etc/hapi-server-dev.sh
@@ -1,21 +1,23 @@
-#!/bin/sh
+#!/bin/bash
 
-APPDIR=/home/ubuntu/server-nodejs-dev
+APPDIR=.
 
-#cpulimit -l 5 -- nice -20 \
-/home/ubuntu/.nvm/versions/node/v18.10.0/bin/node \
-    --max-old-space-size=192 \
-    $APPDIR/server.js \
-    --port 8998 --ignore \
-    --server-ui-include https://raw.githubusercontent.com/hapi-server/servers/master/dev.txt \
-    --server-ui-include https://raw.githubusercontent.com/hapi-server/servers/master/all_.txt \
-    -f $APPDIR/metadata/TestData2.0.json \
-    -f $APPDIR/metadata/TestData2.1.json \
-    -f $APPDIR/metadata/TestData3.0.json \
-    -f $APPDIR/metadata/TestData3.1.json \
-    -f $APPDIR/metadata/URLWatcher.json \
-    -f $APPDIR/metadata/SSCWeb-chunk-ltfloats-parallel.json \
-    -f $APPDIR/metadata/Example0.json \
-    -f $APPDIR/metadata/Example2.json \
-    -f $APPDIR/metadata/CAIO.json \        
-    2>&1
+source /home/ubuntu/.nvm/nvm.sh && nvm use 18
+
+node \
+  --max-old-space-size=256 \
+  $APPDIR/server.js \
+  --port 8998 \
+  --ignore \
+  --server-ui-include https://raw.githubusercontent.com/hapi-server/servers/master/dev.txt  \
+  --server-ui-include https://raw.githubusercontent.com/hapi-server/servers/master/all_.txt \
+  -f $APPDIR/metadata/TestData2.0.json  \
+  -f $APPDIR/metadata/TestData2.1.json  \
+  -f $APPDIR/metadata/TestData3.0.json  \
+  -f $APPDIR/metadata/TestData3.1.json  \
+  -f $APPDIR/metadata/URLWatcher.json   \
+  -f $APPDIR/metadata/SSCWeb-chunk-ltfloats-parallel.json \
+  -f $APPDIR/metadata/Example0.json \
+  -f $APPDIR/metadata/Example2.json \
+  -f $APPDIR/metadata/CAIO.json \
+  2>&1
