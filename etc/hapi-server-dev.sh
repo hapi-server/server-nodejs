d1392d177d2e28e446f4ea429f9b1d7b43a2d478
Ubuntu <rweigel@gmu.edu>
2023-04-11 15:53:52 +0000

    whitelist

--- etc/hapi-server-dev.sh
+++ etc/hapi-server-dev.sh
@@ -11,6 +11,7 @@ node \
   --ignore \
   --server-ui-include https://raw.githubusercontent.com/hapi-server/servers/master/dev.txt  \
   --server-ui-include https://raw.githubusercontent.com/hapi-server/servers/master/all_.txt \
+  --proxy-whitelist $APPDIR/etc/proxy-whitelist.txt \
   -f $APPDIR/metadata/TestData2.0.json  \
   -f $APPDIR/metadata/TestData2.1.json  \
   -f $APPDIR/metadata/TestData3.0.json  \
