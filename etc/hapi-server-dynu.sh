d679fcd0d16e8001da444391332fce9949ea749d
Bob Weigel <rweigel@gmu.edu>
2023-05-21 19:39:39 -0400

    server-ui dir

--- etc/hapi-server-dynu.sh
+++ etc/hapi-server-dynu.sh
@@ -1,10 +1,13 @@
-#!/bin/sh
+#!/bin/bash
 
 USER=/Users/weigel
 APPDIR=$USER/git/hapi/server-nodejs
 
+source ~/opt/anaconda3/etc/profile.d/conda.sh; conda activate
+conda activate base
+
 $USER/.nvm/versions/node/v8.17.0/bin/node \
-    --max-old-space-size=256 \
+    --max-old-space-size=512 \
     $APPDIR/server.js \
     --port 8999 --ignore \
     --server-ui-include https://raw.githubusercontent.com/hapi-server/servers/master/all_.txt \
@@ -13,6 +16,7 @@ $USER/.nvm/versions/node/v8.17.0/bin/node \
     -f $APPDIR/metadata/TestData3.0.json \
     -f $APPDIR/metadata/TestData3.1.json \
     -f $APPDIR/metadata/INTERMAGNET.json \
-    -f $APPDIR/metadata/SSCWeb.json \
-    -f $APPDIR/metadata/SSCWeb-ltfloats.json
+    -f $APPDIR/metadata/SSCWeb-chunk-ltfloats-parallel.json     \
+    -f $APPDIR/metadata/CDAWeb-cdas-cdf-using-pycdf.json        \
+    -f $APPDIR/metadata/CDAWeb-files.json
     2>&1
