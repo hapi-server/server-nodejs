5cfe3f512edd98b942e55a2bf50376e121280388
Bob Weigel <rweigel@gmu.edu>
2024-03-15 06:42:48 -0400

    use node 16

--- etc/hapi-server-dynu.sh
+++ etc/hapi-server-dynu.sh
@@ -6,14 +6,17 @@ APPDIR=$USER/git/hapi/server-nodejs
 source ~/opt/anaconda3/etc/profile.d/conda.sh; conda activate
 conda activate base
 
-$USER/.nvm/versions/node/v12.22.12/bin/node \
+$USER/.nvm/versions/node/v16.20.0/bin/node \
     --max-old-space-size=512 \
     $APPDIR/server.js \
     --port 8999 --ignore \
-    --server-ui-include https://raw.githubusercontent.com/hapi-server/servers/master/all_.txt \
+    --server-ui-include https://hapi-server.org/all_.txt \
+    --proxy-whitelist etc/proxy-whitelist.txt \
     -f $APPDIR/metadata/TestData2.0.json \
     -f $APPDIR/metadata/TestData2.1.json \
     -f $APPDIR/metadata/TestData3.0.json \
     -f $APPDIR/metadata/TestData3.1.json \
-    -f $APPDIR/metadata/SSCWeb-chunk-ltfloats-parallel.json     \
+    -f $APPDIR/metadata/TestData3.2.json \
+    -f $APPDIR/metadata/URLWatcher.json \
+    -f $APPDIR/metadata/SSCWeb-chunk-ltfloats-parallel.json \
     2>&1
--- etc/hapi-server.sh
+++ etc/hapi-server.sh
@@ -1,13 +1,16 @@
 #!/bin/sh
 
-~/.nvm/versions/node/v12.22.12/bin/node \
+~/.nvm/versions/node/v16.20.0/bin/node \
   --max-old-space-size=256 \
   server.js \
   --port 8999 --ignore \
-  --server-ui-include https://raw.githubusercontent.com/hapi-server/servers/master/all_.txt \
+  --server-ui-include https://hapi-server.org/all_.txt \
+  --proxy-whitelist etc/proxy-whitelist.txt \
   -f metadata/TestData2.0.json \
   -f metadata/TestData2.1.json \
   -f metadata/TestData3.0.json \
   -f metadata/TestData3.1.json \
+  -f metadata/TestData3.2.json \
+  -f metadata/URLWatcher.json \
   -f metadata/SSCWeb-chunk.json
   2>&1
