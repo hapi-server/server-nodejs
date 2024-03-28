0b80324d32023cd17540e975271dcae7c36e1aa2
Bob Weigel <rweigel@gmu.edu>
2024-03-28 06:47:17 -0400

    Add --error c/l option

--- bin/Example.py
+++ bin/Example.py
@@ -1,5 +1,5 @@
 # Usage:
-#   python2 Example.py --start START --stop STOP [-params PARAMETERS -fmt FORMAT]
+#   python Example.py --start START --stop STOP [-params PARAMETERS -fmt FORMAT]
 #
 # Generates a HAPI CSV file with a two parameters: a one-column scalar
 # and a 3-column vector. The scalar values are the number of minutes
@@ -15,16 +15,15 @@
 # FORMAT is either 'csv' or 'binary'. The default is 'csv'.
 #
 # Examples:
-#   python2 Example.py --start 1970-01-01T00:00:00.000000Z --stop 1970-01-01T00:10:00.000000Z 
-#   python2 Example.py --fmt binary --start 1970-01-01T00:00:00.000000Z --stop 1970-01-01T00:10:00.000000Z 
-#   python2 Example.py --params Time --start 1970-01-01T00:00:00.000000Z --stop 1970-01-01T00:10:00.000000Z
-#   python2 Example.py --params Time,vector --start 1970-01-01T00:00:00.000000Z --stop 1970-01-01T00:10:00.000000Z
+#   python Example.py --start 1970-01-01T00:00:00.000000Z --stop 1970-01-01T00:10:00.000000Z
+#   python Example.py --fmt binary --start 1970-01-01T00:00:00.000000Z --stop 1970-01-01T00:10:00.000000Z
+#   python Example.py --params Time --start 1970-01-01T00:00:00.000000Z --stop 1970-01-01T00:10:00.000000Z
+#   python Example.py --params Time,vector --start 1970-01-01T00:00:00.000000Z --stop 1970-01-01T00:10:00.000000Z
 
 import sys
 import struct
 import argparse
 import datetime
-import re
 
 # On windows, error is
 #   close failed in file object destructor:
@@ -48,6 +47,7 @@ parser.add_argument('--params',default='')
 parser.add_argument('--start',default='1970-01-01T00:00:00.000000000Z')
 parser.add_argument('--stop',default='1971-01-01T00:00:00.000000000Z')
 parser.add_argument('--fmt',default='csv')
+parser.add_argument('--error',default='')
 
 v      = vars(parser.parse_args())
 epoch  = datetime.datetime(1970,1,1)
@@ -56,10 +56,16 @@ start  = datetime.datetime.strptime(v['start'][0:26],"%Y-%m-%dT%H:%M:%S.%f")
 stop   = datetime.datetime.strptime(v['stop'][0:26],"%Y-%m-%dT%H:%M:%S.%f")
 fmt    = v["fmt"]
 
+if v["error"] == "Code1ExitBeforeDataSent":
+  import time
+  time.sleep(1)
+  sys.stderr.write("1501, Could not connect to server.");
+  sys.exit(1)
+
 if params == '':
-	params_list = ['scalar','vector']
+  params_list = ['scalar','vector']
 else:
-	params_list = params.split(",")
+  params_list = params.split(",")
 
 # Start minute
 mo = int((start-epoch).total_seconds()/60.0)
@@ -79,33 +85,38 @@ if 'scalar' in params_list: scalar = True
 if 'vector' in params_list: vector = True
 
 if fmt == 'binary' and sys.version_info[0] == 3:
-	# Python 3
-	import os
-	stdout = os.fdopen(sys.stdout.fileno(), "wb", closefd=False)
+  # Python 3
+  import os
+  stdout = os.fdopen(sys.stdout.fileno(), "wb", closefd=False)
 
 for i in range(0,mf-mo):
-	d1 = start + datetime.timedelta(minutes=i)
-	if fmt == 'binary':
-		if sys.version_info[0] == 2:
-			sys.stdout.write("%sZ" % d1.isoformat())
-			if scalar == True:
-				sys.stdout.write(struct.pack('<d',mo+i))
-			if vector == True:
-				sys.stdout.write(struct.pack('<ddd',mo+i+1,mo+i+2,mo+i+3))
-		else:
-			# Python 3
-			stdout.write(bytes("%sZ" % d1.isoformat(),'ascii'))
-			if scalar == True:
-				stdout.write(struct.pack('<d',mo+i))
-			if vector == True:
-				stdout.write(struct.pack('<ddd',mo+i+1,mo+i+2,mo+i+3))
-	else:
-		sys.stdout.write("%sZ" % d1.isoformat())
-		if scalar == True:
-			sys.stdout.write(",%d" % (mo+i))
-		if vector == True:
-			sys.stdout.write(",%d,%d,%d" % (mo+i+1,mo+i+2,mo+i+3))
-		sys.stdout.write("\n")
+  d1 = start + datetime.timedelta(minutes=i)
+  if fmt == 'binary':
+    if sys.version_info[0] == 2:
+      sys.stdout.write("%sZ" % d1.isoformat())
+      if scalar == True:
+        sys.stdout.write(struct.pack('<d',mo+i))
+      if vector == True:
+        sys.stdout.write(struct.pack('<ddd',mo+i+1,mo+i+2,mo+i+3))
+    else:
+      # Python 3
+      stdout.write(bytes("%sZ" % d1.isoformat(),'ascii'))
+      if scalar == True:
+        stdout.write(struct.pack('<d',mo+i))
+      if vector == True:
+        stdout.write(struct.pack('<ddd',mo+i+1,mo+i+2,mo+i+3))
+  else:
+    sys.stdout.write("%sZ" % d1.isoformat())
+    if scalar == True:
+      sys.stdout.write(",%d" % (mo+i))
+    if vector == True:
+      sys.stdout.write(",%d,%d,%d" % (mo+i+1,mo+i+2,mo+i+3))
+    sys.stdout.write("\n")
+
+  if v["error"] == "Code1ExitAfterDataSent":
+    sys.stdout.flush()
+    sys.stderr.write("1501, Connection to upstream server ended unexpectedly.");
+    sys.exit(1)
 
 if fmt == 'binary' and sys.version_info[0] == 3:
-	stdout.flush()
+  stdout.flush()
