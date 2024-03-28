1e48a3f2ae86a34a4d68d27300f41d78804dff4e
Bob Weigel <rweigel@gmu.edu>
2024-03-28 07:04:19 -0400

    startup script

--- etc/hapi-server-dynu.sh
+++ etc/hapi-server-dynu.sh
@@ -12,11 +12,13 @@ $USER/.nvm/versions/node/v16.20.0/bin/node \
     --port 8999 --ignore \
     --server-ui-include https://hapi-server.org/all_.txt \
     --proxy-whitelist etc/proxy-whitelist.txt \
-    -f $APPDIR/metadata/TestData2.0.json \
-    -f $APPDIR/metadata/TestData2.1.json \
-    -f $APPDIR/metadata/TestData3.0.json \
-    -f $APPDIR/metadata/TestData3.1.json \
-    -f $APPDIR/metadata/TestData3.2.json \
-    -f $APPDIR/metadata/URLWatcher.json \
+    -f $APPDIR/metadata/TestData2.0.json  \
+    -f $APPDIR/metadata/TestData2.1.json  \
+    -f $APPDIR/metadata/TestData3.0.json  \
+    -f $APPDIR/metadata/TestData3.1.json  \
+    -f $APPDIR/metadata/TestData3.2.json  \
+    -f $APPDIR/metadata/URLWatcher.json   \
     -f $APPDIR/metadata/SSCWeb-chunk-ltfloats-parallel.json \
+    -f $APPDIR/metadata/Example0.json \
+    -f $APPDIR/metadata/Example2.json 
     2>&1
