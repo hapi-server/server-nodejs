6ba9497cdd5cd0b0468b6280e781d83d015ae047
Bob Weigel <rweigel@gmu.edu>
2024-07-07 20:13:54 -0400

    wdc demo

--- metadata/demos/WDC.py
+++ metadata/demos/WDC.py
@@ -1,61 +1,124 @@
 import json
-import requests
-import requests_cache
 
-default_params = {
-    'params': None,  # Dictionary, list of tuples or bytes to send in the query string for the Request
-    'headers': None,  # Dictionary of HTTP Headers to send with the Request
-    'timeout': None,  # Float or a (connect timeout, read timeout) tuple
-    'allow_redirects': True,  # Boolean. Enable/Disable redirection
-    'stream': False,  # if False, the response content will be immediately downloaded
-}
+# Execute curl -O stations.json "https://app-geomag.bgs.ac.uk/wdc/stations"
+# then
+#  python WDC.py --output catalog-all
+# or 
+#  python WDC.py --output catalog
 
-catalog_url = 'https://app-geomag.bgs.ac.uk/wdc/stations'
+output = {"help": "catalog or catalog-combined"}
+import argparse
+parser = argparse.ArgumentParser()
+parser.add_argument(f'--output', **output)
+args = vars(parser.parse_args())
 
-def catalog(catalog_url, session):
-  try:
-      response = session.get(catalog_url)
-      response.raise_for_status()  # raise exception if invalid response
-      try:
-        print(response.from_cache)
-        return json.loads(response.text)
-      except:
-        print('Error. Could not parse catalog. Response given below.')
-        print(response.text)
-  except requests.exceptions.HTTPError as errh:
-      print ("HTTP Error:", errh)
-  except requests.exceptions.ConnectionError as errc:
-      print ("Error Connecting:", errc)
-  except requests.exceptions.Timeout as errt:
-      print ("Timeout Error:", errt)
-  except requests.exceptions.RequestException as err:
-      print ("Something went wrong with the request:", err)
+with open('stations.json', 'r') as f:
+  catalog_dict = json.loads(f.read())
 
 def toISO(year):
   return f'{year}-01-01'
 
-session = requests_cache.CachedSession('cache', expire_after=600)
+def _dataset(id, title, startDate, stopDate, cadence):
+  return {
+    'id': id,
+    'title': title,
+    'info': {
+      'startDate': startDate,
+      'stopDate': stopDate,
+      'cadence': cadence,
+      "maxRequestDuration": "P366D",
+      "description": "",
+      "coordinateSystemSchema": "spase2.4.1",
+      "resourceURL": "https://wdcapi.bgs.ac.uk/doc",
+      "citation": "https://wdcapi.bgs.ac.uk/doc",
+      "contact": "",
+      "x_latitude": 0.0,
+      "x_longitude": 38.77,
+      "x_elevation": 2441,
+      "parameters": [
+          {
+            "name": "Time",
+            "type": "isotime",
+            "length": 17,
+            "units": "UTC",
+            "fill": None,
+            "description": "The UTC date and time for the data from ...",
+            "label": "Date/time"
+          },
+          {
+            "name": "XYZ",
+            "type": "double",
+            "size": [
+              3
+            ],
+            "units": [
+              "nT",
+              "nT",
+              "nT"
+            ],
+            "coordinateSystemName": "Cartesian",
+            "vectorComponents": [
+              "x",
+              "y",
+              "z"
+            ],
+            "fill": "99999.0",
+            "description": "",
+            "label": [
+              "X",
+              "Y",
+              "Z"
+            ]
+          },
+          {
+            "name": "HDZ",
+            "type": "double",
+            "size": [
+              3
+            ],
+            "units": [
+              "nT",
+              "degrees",
+              "nT"
+            ],
+            "coordinateSystemName": "Cylindrical",
+            "vectorComponents": [
+              "r",
+              "theta",
+              "z"
+            ],
+            "fill": "99999.0",
+            "description": "",
+            "label": [
+              "H",
+              "D",
+              "Z"
+            ]
+          }
+        ]
+    }
+  }
 
-catalog_dict = catalog(catalog_url, session)
-catalog_json = json.dumps(catalog_dict, indent=2)
-#print(catalog_json)
-datasets_hour = []
-datasets_minute = []
+datasets = []
 for key, value in catalog_dict.items():
-  dataset = {'id': value['code'], 'title': value['name'], 'info': {'startDate': None, 'stopDate': None}}
+  print(value)
   #print(value['dataAvailability'])
   if 'hour' in value['dataAvailability']:
-    dataset['info']['startDate'] = toISO(value['dataAvailability']['hour']['earliest'])
-    dataset['info']['stopDate'] = toISO(value['dataAvailability']['hour']['latest'])
-    dataset['info']['cadence'] = 'PT1H'
-    datasets_hour.append(dataset)
+    dataset = _dataset(value['code'] + '/hour', value['name'], toISO(value['dataAvailability']['hour']['earliest']), toISO(value['dataAvailability']['hour']['latest']), 'PT1H')
+    datasets.append(dataset)
   if 'minute' in value['dataAvailability']:
-    dataset['info']['startDate'] = toISO(value['dataAvailability']['minute']['earliest'])
-    dataset['info']['stopDate'] = toISO(value['dataAvailability']['minute']['latest'])
-    dataset['info']['cadence'] = 'PT1M'
-    datasets_minute.append(dataset)
+    dataset = _dataset(value['code'] + '/minute', value['name'], toISO(value['dataAvailability']['hour']['earliest']), toISO(value['dataAvailability']['hour']['latest']), 'PT1H')
+    datasets.append(dataset)
+  print(dataset['id'])
+  break
 
-print(json.dumps(datasets_hour, indent=2))
-print(json.dumps(datasets_minute, indent=2))
-print(len(datasets_hour))
-print(len(datasets_minute))
+if args['output'] == 'catalog-all':
+  print(json.dumps(datasets, indent=2))
+
+if args['output'] == 'catalog':
+  datasets = datasets.copy()
+  for dataset in datasets:
+    print(dataset['id'])
+    print(dataset)
+    del dataset['info']
+  print(json.dumps(datasets, indent=2))
